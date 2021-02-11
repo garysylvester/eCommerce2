9f5f00a6d844a7061b4d7caed70926e1d17b0e57
gsylvester <gsylvester@me.com>
2021-02-11 11:24:56 -0800

    adding js for show hide toggles

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function aboutToggle() {
+    var x = document.getElementById("aboutGame");
+    if (x.style.height === "48px") {
+      x.style.height = "auto";
+    } else {
+      x.style.height = "48px";
+    }
+  }
+
+function tileToggleEAD() {
+    var x = document.getElementById("EAD");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    var element = document.getElementById("ead");
+    element.classList.toggle("platformTileSelected");
+  }
+
+  function tileToggleSteam() {
+    var x = document.getElementById("Steam");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    var element = document.getElementById("steam");
+    element.classList.toggle("platformTileSelected");
+  }
+
+  function tileTogglePlaystation() {
+    var x = document.getElementById("Playstation");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    var element = document.getElementById("playstation");
+    element.classList.toggle("platformTileSelected");
+  }
+
+  function tileToggleXbox() {
+    var x = document.getElementById("Xbox");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    var element = document.getElementById("xbox");
+    element.classList.toggle("platformTileSelected");
+  }

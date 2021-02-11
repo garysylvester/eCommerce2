e9a02a43ff894db9617c062b92ac07be438c77a4
gsylvester <gsylvester@me.com>
2021-02-11 12:00:59 -0800

    updating swap text

--- js/main.js
+++ js/main.js
@@ -1,56 +1,63 @@
 function aboutToggle() {
-    var x = document.getElementById("aboutGame");
-    if (x.style.height === "48px") {
-      x.style.height = "auto";
-    } else {
-      x.style.height = "48px";
-    }
+  var x = document.getElementById("aboutGame");
+  if (x.style.height === "48px") {
+    x.style.height = "auto";
+  } else {
+    x.style.height = "48px";
   }
+
+  var x = document.getElementById("learnMore");
+  if (x.innerHTML === "Learn More +") {
+    x.innerHTML = "Collapse -";
+  } else {
+    x.innerHTML = "Learn More +";
+  }
+}
 
 function tileToggleEAD() {
-    var x = document.getElementById("EAD");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-
-    var element = document.getElementById("ead");
-    element.classList.toggle("platformTileSelected");
+  var x = document.getElementById("EAD");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 
-  function tileToggleSteam() {
-    var x = document.getElementById("Steam");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var element = document.getElementById("ead");
+  element.classList.toggle("platformTileSelected");
+}
 
-    var element = document.getElementById("steam");
-    element.classList.toggle("platformTileSelected");
+function tileToggleSteam() {
+  var x = document.getElementById("Steam");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 
-  function tileTogglePlaystation() {
-    var x = document.getElementById("Playstation");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var element = document.getElementById("steam");
+  element.classList.toggle("platformTileSelected");
+}
 
-    var element = document.getElementById("playstation");
-    element.classList.toggle("platformTileSelected");
+function tileTogglePlaystation() {
+  var x = document.getElementById("Playstation");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 
-  function tileToggleXbox() {
-    var x = document.getElementById("Xbox");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var element = document.getElementById("playstation");
+  element.classList.toggle("platformTileSelected");
+}
 
-    var element = document.getElementById("xbox");
-    element.classList.toggle("platformTileSelected");
+function tileToggleXbox() {
+  var x = document.getElementById("Xbox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+
+  var element = document.getElementById("xbox");
+  element.classList.toggle("platformTileSelected");
+}

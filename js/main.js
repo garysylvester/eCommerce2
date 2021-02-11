3bcc8bc629dd05e3bcf000b02d99f84a3f3beef4
gsylvester <gsylvester@me.com>
2021-02-11 14:30:41 -0800

    adding platform choice logic

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function aboutToggle() {
 }
 
 function tileToggleEAD() {
-  var x = document.getElementById("EAD");
+  var x = document.getElementById("eadSection");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -24,10 +24,23 @@ function tileToggleEAD() {
 
   var element = document.getElementById("ead");
   element.classList.toggle("platformTileSelected");
+
+  var element = document.getElementById("steam");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("playstation");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("xbox");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("switch");
+  element.classList.toggle("platformTileInactive");
+
 }
 
 function tileToggleSteam() {
-  var x = document.getElementById("Steam");
+  var x = document.getElementById("steamSection");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -36,10 +49,22 @@ function tileToggleSteam() {
 
   var element = document.getElementById("steam");
   element.classList.toggle("platformTileSelected");
+
+  var element = document.getElementById("ead");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("playstation");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("xbox");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("switch");
+  element.classList.toggle("platformTileInactive");
 }
 
 function tileTogglePlaystation() {
-  var x = document.getElementById("Playstation");
+  var x = document.getElementById("playstationSection");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -48,10 +73,22 @@ function tileTogglePlaystation() {
 
   var element = document.getElementById("playstation");
   element.classList.toggle("platformTileSelected");
+
+  var element = document.getElementById("steam");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("ead");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("xbox");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("switch");
+  element.classList.toggle("platformTileInactive");
 }
 
 function tileToggleXbox() {
-  var x = document.getElementById("Xbox");
+  var x = document.getElementById("xboxSection");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -60,4 +97,41 @@ function tileToggleXbox() {
 
   var element = document.getElementById("xbox");
   element.classList.toggle("platformTileSelected");
+
+  var element = document.getElementById("steam");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("playstation");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("ead");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("switch");
+  element.classList.toggle("platformTileInactive");
 }
+
+
+function tileToggleSwitch() {
+    var x = document.getElementById("switchSection");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  
+    var element = document.getElementById("switch");
+    element.classList.toggle("platformTileSelected");
+    
+    var element = document.getElementById("steam");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("playstation");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("xbox");
+  element.classList.toggle("platformTileInactive");
+
+  var element = document.getElementById("ead");
+  element.classList.toggle("platformTileInactive");
+  }

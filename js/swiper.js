dfcbb3f76ff2dd846c31fa5b38988d3081c7ae48
garysylvester <gsylvester@me.com>
2021-02-16 21:01:03 -0800

    adding modal and slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,3 @@
-var Swipes = new Swiper('.swiper-container', {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 16,
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-      el: '.swiper-pagination',
-  },
-});
+
+
+

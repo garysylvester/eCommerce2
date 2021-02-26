2d04420d8dd6936d869c52e811f55c2a4649862b
gsylvester <gsylvester@me.com>
2021-02-26 14:22:50 -0800

    adding checkout and login

--- js/login.js
+++ js/login.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
         event.preventDefault(); // prevent PageReLoad
 
-       var ValidEmail = $('#username').val() === '123'; // User validate
-var ValidPassword = $('#password').val() === '456'; // Password validate
+       var ValidEmail = $('#username').val() === 'user'; // User validate
+var ValidPassword = $('#password').val() === 'test'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
@@ -17,3 +17,19 @@ var ValidPassword = $('#password').val() === '456'; // Password validate
         }
     });
 });
+
+function expandCheckoutSummary2() {
+    var x = document.getElementById("checkoutSummary2");
+    if (x.style.height === "150px") {
+      x.style.height = "auto";
+    } else {
+      x.style.height = "150px";
+    }
+  
+    var x = document.getElementById("expandCheckoutSummarybtn");
+    if (x.innerHTML === "Show More +") {
+      x.innerHTML = "Show Less -";
+    } else {
+      x.innerHTML = "Show More +";
+    }
+  }
